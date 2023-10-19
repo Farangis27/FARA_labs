60d79a0f4ebce7a45dde023ced233a3a927b5bbb
Farangisi Huseni <145720899+Farangis27@users.noreply.github.com>
2023-10-19 13:51:20 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-a=input("enter the string:")
-b=a.replace(" ","")
-print(b)
+with open('transcript.txt', 'r') as file:
+    content = file.read()
+    words = content.split()
+    word_count = len(words)
+    print(f"The word count of my text file is {word_count} words")
 
-x=input("enter the string")
-b=(x[::-1])
-if x==b:
-    print("palindrome")
-else:
-    print("not palindrome")
+    # replace spaces with nothing
+    characters = content.replace("","")
+    # Get the length of the data
+    number_of_characters = len(content)
+    print(f"Number of characters in my text file is {number_of_characters}")
+
+    # Get the average word length in the file
+    average_word_length = word_count/number_of_characters
+    print(f"The average word length is {average_word_length}")
+
+    # Get the frequency of each word in the file
+    for word in words:
+        freq = words.count(word)
+        print(word, freq)
